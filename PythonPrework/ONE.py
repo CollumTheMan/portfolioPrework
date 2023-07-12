cebfc3f2837dc77ab9de10f127a26cc152d6c037
Collum Powers <collumpowers@Collums-MBP.attlocal.net>
2023-07-12 19:46:04 -0400

    CoolioSquared

--- PythonPrework/ONE.py
+++ PythonPrework/ONE.py
@@ -28,3 +28,30 @@ def max_num_in_list(a_list):
     
 print(max_num_in_list([1,2,2,4,5,6,7,7,1231]))
 
+# Question 4
+# Write a function to return if the given year is a leap year. A leap year is divisible by 4, but not divisible by 100 unless it is also divisible by 400. The return should be boolean Type (true/false). def is_leap_year(a_year):
+
+def is_leap_year(a_year):
+    if ((a_year % 4) != 0):
+        return False
+    else:
+        if (a_year % 100) != 0 or (a_year % 400) == 0:
+            return True
+        else:
+            return False
+
+print(is_leap_year(4000))
+
+
+# Question 5
+
+# Write a function to check to see if all numbers in the list are consecutive numbers. For example, [2,3,4,5,6,7] are consecutive numbers, but [1,2,4,5] are not consecutive numbers. The return should be boolean Type. def is_consecutive(a_list):
+
+def is_consecutive(a_list):
+    for x in range(len(a_list) - 1):
+        if a_list[x] != a_list[x  + 1] - 1:
+            return False
+            
+    return True
+    
+print(is_consecutive(a_list))
